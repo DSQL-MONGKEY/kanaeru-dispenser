44713bf64302b8c2049e29b88201b730eb9ee3da
Dimpfe <dimasprasetyo7696@gmail.com>
2025-06-06 23:27:00 +0700

    feat<>: create mqtt client lib setup

---
+++ src/lib/mqtt-client.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+import  mqtt  from 'mqtt';
+const MQTT_BROKER_URL = process.env.MQTT_BROKER_URL || 'wss://broker.hivemq.com:8884/mqtt';
+
+const options = {
+   username: process.env.MQTT_USERNAME,
+   password: process.env.MQTT_PASSWORD,
+   reconnectPeriod: 1000
+}
+
+const mqttClient = mqtt.connect(MQTT_BROKER_URL, options);
+mqttClient.on('connect', () => {
+   console.log('MQTT Connected to HiveMQ');
+})
+mqttClient.on('error', (err) => {
+   console.log('MQTT Connected Error: ', err)
+})
+
+mqttClient.on('message', function (topic, message) {
+    // called each time a message is received
+      console.log('Received message:', topic, message.toString());
+});
+
+
+export default mqttClient;
