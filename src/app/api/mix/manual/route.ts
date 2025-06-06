628d2f61fadc51888109e325dad3afc8f1c51f1a
Dimpfe <dimasprasetyo7696@gmail.com>
2025-06-06 23:26:44 +0700

    feat<>: create api handler

--- src/app/api/mix/manual/route.ts
+++ src/app/api/mix/manual/route.ts
@@ -1,3 +1,4 @@
+import mqttClient from "@/lib/mqtt-client";
 import { supabase } from "@/lib/supabase/server";
 import { NextResponse } from "next/server";
 
@@ -6,34 +7,96 @@ export async function POST(req: Request) {
       const body = await req.json();
 
       const {
-         name,
-         phone,
-         email,
-         address,
+         recipe_name,
+         total_ml,
+         composition,
       } = body;
 
-      const { data: climberData, error: climberError } = await supabase
-         .from('climber_users')
-         .insert({
-            name,
-            phone,
-            email,
-            address
-         })
-         .select();
+      const pumpNumbers = composition.map((c: any) => c.pump_number);
 
-         if(climberError) {
-            return NextResponse.json({
-               success: false,
-               error: 'Error creating climber user',
-               details: climberError.message
-            }, { status: 500 });
-         }
+      const { data:pumpSettings, error:pumpSettingsError } = await supabase
+         .from('pump_settings')
+         .select('pump_number, flow_rate')
+         .in('pump_number', pumpNumbers);
 
+      if(!pumpSettings || pumpSettingsError) {
          return NextResponse.json({
-            success: true,
-            data: climberData,
-         }, { status: 201 });
+         success: false,
+         error: 'Request Failed',
+         details: pumpSettingsError?.message || 'Pump number not found',
+         }, { status: 404 });
+      }
+
+      const flowRateMap = pumpSettings.reduce((map: any, setting: any) => {
+         map[setting.pump_number] = setting.flow_rate;
+         return map;
+      }, {});
+
+      const calculatedComposition = composition.map((c:any) => {
+         const ml_portion = (c.percentage * total_ml) / 100;
+         const active_time = (ml_portion / flowRateMap[c.pump_number]) * 1000;
+
+         return {
+            ...c,
+            ml_portion,
+            active_time
+         }
+      });
+      
+      const mqttPayload = {
+         recipe_name,
+         total_ml,
+         composition: calculatedComposition
+      }
+      mqttClient.publish(
+         'multidispenser/mix',
+         JSON.stringify(mqttPayload),
+         { qos: 1 },
+         (err) => {
+            if(err) console.error('Error publishing MQTT: ', err);
+         }
+      );
+
+      const { data:mixLog, error:mixLogError } = await supabase
+         .from('mix_logs')
+         .insert({
+            mode: 'manual',
+            recipe_id: null,
+            total_volume: total_ml
+         })
+         .select()
+         .single();
+
+      if(!mixLog || mixLogError) {
+         return NextResponse.json({
+            success: false,
+            error: 'Failed to send logs',
+            details: mixLogError?.message || 'Logs error',
+         }, { status: 500 });
+      }
+
+      const detailsToInsert = calculatedComposition.map((pump: any) => ({
+         mix_log_id: mixLog.id,
+         pump_number: pump.pump_number,
+         volume_ml: pump.ml_portion
+      }));
+
+      const { error:mixLogDetailError } = await supabase
+         .from('mix_log_details')
+         .insert(detailsToInsert)
+
+      if(mixLogDetailError) {
+         NextResponse.json({
+            success: false,
+            error: 'Failed to send log details',
+            message: mixLogDetailError?.message || 'Error log details'
+         })
+      }
+
+      return NextResponse.json({
+         success: true,
+         data: mixLog,
+      }, { status: 201 });
 
    } catch(error) {
 
