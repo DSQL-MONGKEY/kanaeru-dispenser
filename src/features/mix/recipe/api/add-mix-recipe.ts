d7a30c1b263c584b7ec7ea97aa9b1afa8e6c7377
Dimpfe <dimasprasetyo7696@gmail.com>
2025-07-04 11:26:08 +0700

    feat<>: change props types optional

--- src/features/mix/recipe/api/add-mix-recipe.ts
+++ src/features/mix/recipe/api/add-mix-recipe.ts
@@ -1,22 +1,20 @@
-
-
 type TMixRecipe = {
-   recipeId: number;
-   totalMl: number;
-}
+  recipeId: number;
+  totalMl?: number;
+};
 
-export const addMixRecipe = async ({ totalMl, recipeId }:TMixRecipe) => {
-   const response = await fetch(`/api/mix/recipe`, {
-      method: 'POST',
-      headers: {
-         'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-         recipe_id: recipeId,
-         total_ml: totalMl,
-      }),
-   });
-   const data = await response.json();
+export const addMixRecipe = async ({ totalMl, recipeId }: TMixRecipe) => {
+  const response = await fetch(`/api/mix/recipe`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      recipe_id: recipeId,
+      total_ml: totalMl
+    })
+  });
+  const data = await response.json();
 
-   return data;
-}
+  return data;
+};
