bbfb5f404e43f42524590257fed9f7907ce6da2c
Dimpfe <dimasprasetyo7696@gmail.com>
2025-07-04 13:16:52 +0700

    feat<>: change types to optional

--- src/features/recipes/api/add-recipe.ts
+++ src/features/recipes/api/add-recipe.ts
@@ -1,28 +1,26 @@
-
-
 type TRecipe = {
-   name: string;
-   pump1: number;
-   pump2: number;
-   pump3: number;
-}
+  name?: string;
+  pump1?: number;
+  pump2?: number;
+  pump3?: number;
+};
 
-export const addRecipe = async ({ name, pump1, pump2, pump3 }:TRecipe  ) => {
-   const response = await fetch(`/api/recipes`, {
-      method: 'POST',
-      headers: {
-         'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-         name,
-         composition: [
-            {pump_number: 1, percentage: pump1},
-            {pump_number: 2, percentage: pump2},
-            {pump_number: 3, percentage: pump3},
-         ],
-      }),
-   });
-   const data = await response.json();
+export const addRecipe = async ({ name, pump1, pump2, pump3 }: TRecipe) => {
+  const response = await fetch(`/api/recipes`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name,
+      composition: [
+        { pump_number: 1, percentage: pump1 },
+        { pump_number: 2, percentage: pump2 },
+        { pump_number: 3, percentage: pump3 }
+      ]
+    })
+  });
+  const data = await response.json();
 
-   return data;
-}
+  return data;
+};
