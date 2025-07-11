60b3198b1e4ce3a67194385949bd38b557c7e8b3
Dimpfe <dimasprasetyo7696@gmail.com>
2025-07-11 21:10:06 +0700

    feat<>: remove unused file api handler

--- src/features/pump-settings/api/delete-climber.ts
+++
@@ -1,9 +0,0 @@
-export const deleteClimber = async (id: string) => {
-  const response = await fetch(`/api/climber-users/${id}`, {
-    method: 'DELETE'
-  });
-
-  const data = await response.json();
-
-  return data;
-};
